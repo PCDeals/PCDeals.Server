dc3940e58c1a2b4f80c160efb40f2fc1b1d1eda5
Mathew Kurian <bluejamesbond@gmail.com>
2016-01-02 03:36:19 -0600

    Param obey_over18 defaulted to false

--- libs/sources/reddit/index.js
+++ libs/sources/reddit/index.js
@@ -16,7 +16,8 @@ export default class Reddit extends Source {
       subreddit: 'buildapcsales',
       after: '',
       time: 'all',
-      sort: 'hot'
+      sort: 'hot',
+      obey_over18: false
     }, opts);
 
     opts.format = '.json';
@@ -26,8 +27,8 @@ export default class Reddit extends Source {
 
     const url = `${[HOST, opts.subreddit, opts.sort, opts.format].join('/')}?${querystring.stringify(opts)}`;
     return request({url, json: true})
-      .then(res => res.data.children)
-      .then(res => res.map(post=> post.data));
+        .then(res => res.data.children)
+        .then(res => res.map(post=> post.data));
   }
 
   normalize(posts, opts) {
@@ -38,19 +39,19 @@ export default class Reddit extends Source {
     posts = JSON.parse(JSON.stringify(posts));
     return new Promise((resolve, reject) => {
       async.each(Array.isArray(posts) ? posts : [posts],
-        (post, callback) => {
-          post = Object.assign(post, extract(post.title));
-          if (opts.images) {
-            return this.fetchImages(post.title)
-              .then(links => {
-                links = !links.length ? [Source.DEFAULT_IMAGE] : links;
-                post.images = links;
-                post.image = links[0];
-              }).then(()=> callback())
-              .catch(err => callback());
-          }
-          return callback();
-        }, () => resolve(posts));
+          (post, callback) => {
+            post = Object.assign(post, extract(post.title));
+            if (opts.images) {
+              return this.fetchImages(post.title)
+                  .then(links => {
+                    links = !links.length ? [Source.DEFAULT_IMAGE] : links;
+                    post.images = links;
+                    post.image = links[0];
+                  }).then(()=> callback())
+                  .catch(err => callback());
+            }
+            return callback();
+          }, () => resolve(posts));
     });
   }
 
@@ -67,29 +68,29 @@ export default class Reddit extends Source {
     emitter.cancel = cancel;
 
     this.fetch(opts)
-      .then(posts => {
-        history = _.pluck(posts, 'id');
+        .then(posts => {
+          history = _.pluck(posts, 'id');
 
-        setTimeout(() => {
+          setTimeout(() => {
 
-          if (cancel) {
-            return;
-          }
+            if (cancel) {
+              return;
+            }
 
-          cancel = setInterval(() => {
-            this.fetch(opts)
-              .then(posts => {
-                const n = _.pluck(posts, 'id');
-                const t = _.indexBy(posts, 'id');
-                const m = _.difference(n, history);
+            cancel = setInterval(() => {
+              this.fetch(opts)
+                  .then(posts => {
+                    const n = _.pluck(posts, 'id');
+                    const t = _.indexBy(posts, 'id');
+                    const m = _.difference(n, history);
 
-                history = _.uniq(_.union(history, n));
+                    history = _.uniq(_.union(history, n));
 
-                m.forEach(id => emitter.emit('new-post', t[id]));
-              }).catch(err => console.error(err.message));
-          }, interval);
-        }, 4000);
-      });
+                    m.forEach(id => emitter.emit('new-post', t[id]));
+                  }).catch(err => console.error(err.message));
+            }, interval);
+          }, 4000);
+        });
 
     return emitter;
   }
